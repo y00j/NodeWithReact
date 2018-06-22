aad4147ef85d4d8f03c5306acc149dd1d6a71331
Yujie Zhu <yujiezhu718@gmail.com>
2018-06-22 16:23:35 -0700

    changed greeting

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 // app is a configuration to listen to requests coming in from node and route it to different route handlers
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: "stuff" });
 });
 
 
