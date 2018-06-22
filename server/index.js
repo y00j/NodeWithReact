ce6a5d0e74125f7b96ae1ad290f651f01264b215
Yujie Zhu <yujiezhu718@gmail.com>
2018-06-22 16:14:18 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//app is a running express server. most projects will just use just one of these
+// app is a configuration to listen to requests coming in from node and route it to different route handlers
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+
+const PORT = process.env.PORT || 5000; //dynamic port binding with heroku --> sets default to 5000
+app.listen(PORT); 
