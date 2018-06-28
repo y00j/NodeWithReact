ced42069730fafa7c78b6a25a325e264cfd542b2
Yujie Zhu <yujiezhu718@gmail.com>
2018-06-28 14:06:15 -0700

    updating

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
+passport.use(
+  new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'] //we are asking google for their profile and email -> there are more too
+  })
+);
 //app is a running express server. most projects will just use just one of these
 // app is a configuration to listen to requests coming in from node and route it to different route handlers
 
-app.get('/', (req, res) => {
-  res.send({ bye: "stuff" });
-});
-
-
 const PORT = process.env.PORT || 5000; //dynamic port binding with heroku --> sets default to 5000
-app.listen(PORT); 
+app.listen(PORT);
