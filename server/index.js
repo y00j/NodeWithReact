0c254d6b692bade61b0de92b0892f921a11b0fd5
Yujie Zhu <yujiezhu718@gmail.com>
2018-06-29 17:48:31 -0700

    serializeUser and deserializeUser

--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken) => {
-      console.log(accessToken);
-    }
-  )
-);
+require('./routes/authroutes')(app);
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'] //we are asking google for their profile and email -> there are more too
-  })
-);
-//app is a running express server. most projects will just use just one of these
-// app is a configuration to listen to requests coming in from node and route it to different route handlers
-
-const PORT = process.env.PORT || 5000; //dynamic port binding with heroku --> sets default to 5000
+const PORT = process.env.PORT || 5000;
+//dynamic port binding with heroku --> sets default to 5000
 app.listen(PORT);
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const passport = require('passport');
+
+
+module.exports = (app) => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email'] //we are asking google for their profile and email -> there is a long list of fields we can get
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+  //app is a running express server. most projects will just use just one of these
+  // app is a configuration to listen to requests coming in from node and route it to different route handlers
+};
