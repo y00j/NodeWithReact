40a4aef0d25a537875943961a29dc6d04ecba2e3
Yujie Zhu <yujiezhu718@gmail.com>
2018-07-07 15:49:49 -0700

    finished bootstrapping user, and billing cycle

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -9,5 +9,6 @@ export const fetchUser = () => async (dispatch) => {
 
 export const handleToken = (token) => async (dispatch) => {
 	const res = await axios.post('/api/stripe', token);
+	
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
